6985e8cf40846ae5b44e41fe53f96f27d7c554a4
SYED ALI MEHDI RIZVI <s.a.m.rizvi@hotmail.com>
2024-05-01 23:10:14 +0500

    first commit

---
+++ changingGuestList.ts
@@ -0,0 +1,20 @@
+
+
+let Invitee = ['Zayghum', 'Eric', 'Jhonson'];
+let Status = ['Deceased','Alive','Alive'];
+let Invitedtodinner = ['Not invited','Invited','Invited'];
+console.log('\n'+'\x1b[4m'+'List of invitees who will attend the dinner to night'+'\n');
+console.log(
+'Name of Invited Person :'+ Invitee[0] + '\n'+'Death Status :'+ Status[0] + '\n' + 'Status :' +Invitedtodinner[0] +
+'\n\n'+ 'Name of Invited Person :'+ Invitee[1] + '\n'+'Death Status :'+ Status[1] + '\n' + 'Status :' +Invitedtodinner[1] +
+'\n\n'+'Name of Invited Person :'+ Invitee[2] + '\n'+'Death Status :'+ Status[2] + '\n' + 'Status :' +Invitedtodinner[2] )
+
+
+console.log('\n'+'Attension : Unfortunatly, due to deceased of Mr.Zayghum cant make to attend the dinner to night');
+console.log('\n'+'\x1b[4m'+'Revised list of invitees who will attend the dinner to night');
+Invitee[0]='Ali Mehdi';
+Status[0]='Alive'
+console.log(
+    'Name of Invited Person :'+ Invitee[0] + '\n'+'Death Status :'+ Status[0] + '\n' + 'Status :' +Invitedtodinner[0] +
+    '\n\n'+ 'Name of Invited Person :'+ Invitee[1] + '\n'+'Death Status :'+ Status[1] + '\n' + 'Status :' +Invitedtodinner[1] +
+    '\n\n'+'Name of Invited Person :'+ Invitee[2] + '\n'+'Death Status :'+ Status[2] + '\n' + 'Status :' +Invitedtodinner[2] )
